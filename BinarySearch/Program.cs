3be431b9a053ae1d4a34ea1078604f5ef3b21b34
Shahid <abdul.baset@aci-bd.com>
2018-06-26 15:35:26 +0600

    Selection sort

---
+++ BinarySearch/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] rolls = {2, 3, 4, 6, 8, 9, 13, 25, 31, 33, 43, 44, 45, 47};
+            int result = Binary_Search(rolls, 31);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+
+        public static int Binary_Search(int[] arr,int x)
+        {
+            int l = arr.Length;
+            int left = 0;
+            int right = l - 1;
+
+            while (left <= right)
+            {
+                var mid = (left + right) / 2;
+                if (arr[mid]==x)
+                {
+                    return mid;
+                }
+                if (arr[mid] <x)
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+            return -1;
+        }
+    }
+}
---
+++ SortAlgorithm/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] rolls = {4, 20, 5, 16, 18, 13, 9, 15, 10};
+
+            var result_array = SelectionSort(rolls);
+            Console.WriteLine(string.Join("<", result_array));
+            Console.ReadLine();
+        }
+
+        public static int[] SelectionSort(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i= 0;  i < n; i++)
+            {
+                var minIndex = i;
+
+                for (int j = i+1; j < n; j++)
+                {
+                    if (arr[j]< arr[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                }
+
+                if (minIndex != i)
+                {
+                    var temp = arr[i];
+                    arr[i] = arr[minIndex];
+                    arr[minIndex] = temp;
+                }
+            }
+            return arr;
+        }
+    }
+}
